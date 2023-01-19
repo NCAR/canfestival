c97f8201e0dfea8ef167a99cf108422bc53f8f85
Chris Burghart <burghart@ucar.edu>
2023-01-19 15:55:23 -0700

    Expand the error message from objdictgen_warn.sh

--- objdictgen/objdictgen_warn.sh
+++ objdictgen/objdictgen_warn.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-echo "canfestival's objdictgen was not installed: default Python was not version 2"
+echo "==========="
+echo "ERROR: CanFestival's real \"objdictgen\" was not installed because"
+echo "it requires the default Python to be version 2."
+
+# If they gave a source object dictionary file and target, let them
+# know the target will not be generated.
+if [ "$#" -eq 2 ]; then
+	echo
+	echo "$2 will NOT be generated from $1"
+fi
+
+echo "==========="
+
 exit 1
